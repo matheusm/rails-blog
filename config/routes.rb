a62894bf6fb70de4a8d75298082f52756b2346da
Matheus Martins <matheusms280100@gmail.com>
2024-04-17 15:00:13 -0300

    add update post

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   root "blog_posts#index"
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  patch "/blog_posts/:id", to: "blog_posts#update"
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 end
